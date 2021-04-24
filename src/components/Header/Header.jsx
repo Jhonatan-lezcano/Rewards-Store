dc93c54f458e8f995c50cb33af73d7cf8e44b3b4
jhonatan lezcano <jhonatanlezcano.plv@gmail.com>
2021-04-24 00:39:58 -0500

    prueba styled components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Title } from "./Header";
+
+const Header = () => {
+  return (
+    <div>
+      <Title>Hello world!</Title>
+    </div>
+  );
+};
+
+export default Header;
