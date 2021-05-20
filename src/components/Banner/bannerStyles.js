6c19466987aa497e315393e92fc286356e6db7a7
jhonatan lezcano <jhonatanlezcano.plv@gmail.com>
2021-05-20 18:32:28 -0500

    ultimos ajustes

--- src/components/Banner/bannerStyles.js
+++ src/components/Banner/bannerStyles.js
@@ -8,27 +8,14 @@ export const ContainerBanner = styled.div`
   }
 `;
 
-const movimiento = keyframes`
-  0%{
-    transform: translateY(0);
-  }
-  50%{
-    transform: translateY(30px);
-  }
-  100%{
-    transform: translateY(0);
-  }
-`;
-
 export const TitleRewardsStore = styled.div`
   position: absolute;
   bottom: 25%;
   left: 5%;
-  animation: ${movimiento} 2.5s linear infinite;
+
   h1 {
     font-family: "Newsreader", serif;
     font-size: 74px;
-
     color: #fff;
     text-shadow: 4px 4px 4px gray;
   }
